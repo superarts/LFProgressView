9b2668bc04b1f0c85518934684a771aeee6fa51f
Leo <superartstudio@gmail.com>
2014-06-13 12:28:45 +1000

    1.3.1

--- LDProgressView/LFProgressView.h
+++ LDProgressView/LFProgressView.h
@@ -1,5 +1,9 @@
 #import "LDProgressView.h"
 
+#define LFProgressStripes	LDProgressStripes
+#define LFProgressGradient	LDProgressGradient
+#define LFProgressSolid		LDProgressSolid
+#define LFProgressType		LDProgressType
 
 //	TODO: subclassing LDProgress instead of just using name wrap
 @interface LFProgressView : LDProgressView
